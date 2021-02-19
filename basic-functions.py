46b7da2147e0fe75dc722f56ec886949c57c1372
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-19 15:16:08 -0500

    messing around with some basic arithmetic functions

---
+++ basic-functions.py
@@ -0,0 +1,17 @@
+def addition(x, y):
+   return x + y 
+
+def subtraction(x, y):
+   return x - y
+
+def multiplication(x, y):
+   return x * y
+
+def division(x, y):
+   return x / y
+
+print(addition(5, 2))
+print(addition(60, 40))
+print(subtraction(32, 10))
+print(multiplication(10, 10))
+print(division(20, 4))
--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
 print("hello world") # this is a print function
+
+msg = "lowercase"
+new = msg.capitalize()
+print(new)
