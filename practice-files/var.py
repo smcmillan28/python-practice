5154ec20d7ca21d1fd048f445941ab4f6ef606ac
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-19 21:50:40 -0500

    wrote some lists

--- practice-files/var.py
+++ practice-files/var.py
@@ -3,6 +3,33 @@ print(i)
 i = i + 1
 print(i)
 
+print("---------")
+
 s = '''This is a multi-line string.
 This is the second line.'''
 print(s)
+
+print("---------")
+
+newList = ["apple", "banana", "cherry"]
+newList.insert(1, "grape")
+newList.append("blueberry")
+for i in range(len(newList)):
+    print(newList[i])
+
+print("---------")
+
+listCopy = newList.copy()
+listCopy.remove("banana")
+print(listCopy)
+
+print("---------")
+
+print("This is a simple dictionary item.")
+myCar = {
+    "make": "Chevy",
+    "model": "Impala",
+    "year": "2016"
+}
+print(myCar)
+print("I drive a/an " + myCar["model"])
